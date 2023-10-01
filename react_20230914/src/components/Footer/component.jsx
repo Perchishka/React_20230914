e0b31bf7cbe9b72d8e48fa169a3af11429c4553b
Perchishka <ekaterina_perets@mail.ru>
2023-10-01 21:47:19 +0300

    button styles

--- react_20230914/src/components/Footer/component.jsx
+++ react_20230914/src/components/Footer/component.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import styles from "./styles.module.css";
 import { Link } from "../BaseComponents/Link/link";
-import FooterLink from "../FooterLink/styles.module.css"
+import FooterLink from "../FooterLink/styles.module.css";
 
 export const Footer = ({ links }) => {
-  const madeBy = {name:"Perchishka", path:"https://github.com/Perchishka"};
+  const madeBy = { name: "Perchishka", path: "https://github.com/Perchishka" };
   return (
     <div className={styles.footer}>
       <ul className={styles.linkList}>
@@ -16,7 +16,7 @@ export const Footer = ({ links }) => {
       </ul>
       <div className={styles.dev}>
         <span className={styles.madeBy}>Made by: </span>
-        <Link className ={FooterLink.footer_link} link={madeBy} />     
+        <Link className={FooterLink.footer_link} link={madeBy} />
       </div>
     </div>
   );
