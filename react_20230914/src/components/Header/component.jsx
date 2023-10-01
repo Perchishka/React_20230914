a52ef2b5f205cdf01c126ee62c853aefa06454a3
Perchishka <ekaterina_perets@mail.ru>
2023-10-01 21:52:31 +0300

    span was removed

--- react_20230914/src/components/Header/component.jsx
+++ react_20230914/src/components/Header/component.jsx
@@ -6,11 +6,8 @@ export const Header = () => {
   const madeBy = {name:"Sticky Header", path:"https://www.w3schools.com/howto/howto_js_sticky_header.asp"};
     return (
         <div className = {styles.header}>
-          <div className={styles.header__container}>
             <div className="devs">
-              <span>Sticky Header</span>
               <Link link ={madeBy}></Link>
-            </div>
           </div>
         </div>
     )
